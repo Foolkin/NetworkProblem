71c3a00977d09e5a2ff469013e24dddea427764f
Foolkin <jeka321@yandex.ru>
2014-04-18 19:39:51 +0300

    Added class Client

---
+++ Client/src/main/java/Client.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * This class contains socket whith wich you can connect to server
+ */
+public class Client {
+}
