cd9817e4823a4db6db333a347b8d2d944e7411e0
Foolkin <jeka321@yandex.ru>
2014-04-18 19:42:46 +0300

    Added class ReceiverRunnable, SenderRunnable. Complete Client

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -7,4 +7,48 @@ import java.util.concurrent.Executors;
  * This class contains socket whith wich you can connect to server
  */
 public class Client {
+    Socket socket;
+
+    public Client(String url, int port){
+        initSocket(url, port);
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client("127.0.0.1", 8189);
+    }
+
+    /**
+     *  Starts session between client and Server
+     *  Multithreaded. Uses ExecutorServise.
+     */
+    public void startClient(){
+        try {
+            try {
+                ExecutorService executor = Executors.newCachedThreadPool();
+
+                executor.execute(new SenderRunnable(socket));
+                executor.execute(new RecivierRunnable(socket));
+
+                executor.shutdown();
+            } finally {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Create Socket object with current url address and port
+     * @param port - int type. Port number which ServerSocket will listen.
+     */
+    public void initSocket(String url, int port){
+        try{
+            socket = new Socket(url, port);
+        } catch (IOException e){
+            System.err.println(e);
+        }
+    }
+
 }
